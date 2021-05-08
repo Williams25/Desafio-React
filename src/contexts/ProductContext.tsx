4f4a88fdde61f9009867fbc96d9e723c0ef9fc71
Williams25 <william.souza.timao@hotmail.com>
2021-05-07 23:04:19 -0300

    context de produto, foi criado metodos para interagir com a api

--- src/contexts/ProductContext.tsx
+++ src/contexts/ProductContext.tsx
@@ -2,7 +2,19 @@ import { createContext, ReactNode, useState, useEffect } from "react";
 import { product } from "../services";
 
 interface ProductContextData {
-  handleToSaveProduct: ({ name, price, category }: ProductSave) => void;
+  handleToSaveProduct: ({ name, price, category, code }: ProductSave) => void;
+  handleToUpdateProduct: ({
+    name,
+    price,
+    category,
+    code,
+    id,
+  }: ProductSave) => void;
+  productExists: ({ collunm, value }) => void;
+  loadingProducts: () => void;
+  removeProdut: ({ id }) => void;
+  toHaveProduct: ProductSave[];
+  productsList: ProductSave[];
 }
 
 interface ProductProviderProps {
@@ -14,16 +26,78 @@ interface ProductSave {
   name: string;
   price: string;
   category: string;
+  code: number;
 }
 
 export const ProductContext = createContext({} as ProductContextData);
 
 export const ProductProvider = ({ children }: ProductProviderProps) => {
-  const handleToSaveProduct = ({ name, price, category }: ProductSave) => {
+  const [toHaveProduct, setToHaveProduct] = useState<ProductSave[]>([]);
+  const [productsList, setProductsList] = useState<ProductSave[]>([]);
+
+  const loadingProducts = () => {
     product
-      .create({ name, price, category })
+      .findAll()
       .then((res) => {
         const { data } = res;
+        setProductsList(data);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  };
+
+  const productExists = ({ collunm, value }) => {
+    product
+      .findByCollunm(collunm, value)
+      .then((res) => {
+        const { data } = res;
+        setToHaveProduct(data);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  };
+
+  const handleToSaveProduct = ({
+    name,
+    price,
+    category,
+    code,
+  }: ProductSave) => {
+    product
+      .create({ name, price, category, code })
+      .then((res) => {
+        loadingProducts();
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  };
+
+  const handleToUpdateProduct = ({
+    name,
+    price,
+    category,
+    code,
+    id,
+  }: ProductSave) => {
+    product
+      .update({ name, price, category, code, id })
+      .then((res) => {
+        loadingProducts();
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  };
+
+  const removeProdut = ({ id }) => {
+    console.log("context ", id);
+    product
+      .delete({ id })
+      .then((res) => {
+        loadingProducts();
       })
       .catch((err) => {
         console.error(err);
@@ -31,7 +105,17 @@ export const ProductProvider = ({ children }: ProductProviderProps) => {
   };
 
   return (
-    <ProductContext.Provider value={{ handleToSaveProduct }}>
+    <ProductContext.Provider
+      value={{
+        handleToSaveProduct,
+        productExists,
+        toHaveProduct,
+        productsList,
+        loadingProducts,
+        handleToUpdateProduct,
+        removeProdut,
+      }}
+    >
       {children}
     </ProductContext.Provider>
   );
